f15ed2a124803fd62a5c61fde1061d2e860792b5
tanimoto shoma <tanimoto.shoma@compass-japan.com>
2026-06-08 15:14:14 +0900

    add sample test

---
+++ demo/src/test/java/com/example/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
